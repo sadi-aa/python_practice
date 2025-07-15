cb141d01eb0ff2e3c6a1dc404aa4dca8e0358879
Sadia <abdullahsadia851@gmail.com>
2025-07-15 14:57:10 +0500

    loop file created

---
+++ loop.py
@@ -0,0 +1,12 @@
+#Print numbers from 1 to 10 using a for loop
+i=0
+for i in range(1,11):
+    print(i)
+
+
+#Use a while loop to print "Hello" 5 times.
+i=0
+while i<5:
+    print("hello")
+    i=i+1
+    
