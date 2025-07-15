659c7ffa156a278f739e3a02826b5c9c211d61c7
Sadia <abdullahsadia851@gmail.com>
2025-07-15 14:21:29 +0500

    tuple file created

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#Count how many times 10 appears in this tuple:
+t=(10,40,70,10,20,10)
+print(t.count(10))
+
+#Find the index of "banana" in the tuple
+a=("orange","apple",'banana')
+print(a.index("banana"))
+
+#Slice a tuple to get the last 3 items:
+t = (1, 2, 3, 4, 5, 6)
+print(t[3:6])
