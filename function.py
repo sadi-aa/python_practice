7a5a5e30dee610c144c963abacbbb28e0a20230c
Sadia <abdullahsadia851@gmail.com>
2025-07-15 17:29:24 +0500

    function file created

---
+++ function.py
@@ -0,0 +1,11 @@
+#Write a function to add two numbers.
+def fun(a,b):
+    return a+b
+result=fun(3,6)
+print(result)
+
+#Write a function that returns the square of a number
+def square(a):
+    return a*a
+results=square(4)
+print(results)
