7ddca198c62220c3f2ac0b8f4ae3a250d65a0d50
Sadia <abdullahsadia851@gmail.com>
2025-07-15 13:52:57 +0500

    variable2 file created

--- variable.py
+++ variable.py
@@ -1,3 +1,11 @@
 #Convert the string "123" to an integer and add 10 to it.
-a='123'
-a=int(a=10)
+a='13'
+a=int(a)
+print(a+10)
+#Check if the variable a = None behaves like False in an if-condition.
+a=None
+if a:
+    print("not none")
+else:
+    print("is none")
+
---
+++ variable2.py
@@ -0,0 +1,13 @@
+#Write a program to store a user's full name, age, and email in separate variables and print a formatted message.
+a='sadia'
+b=23
+c='abdullahsadia@gamil.com'
+print( f"my name is {a}. i am {b} years old and my gmail is {c}")
+
+#Using only one line, assign values to 3 variables: x=1, y=2, and z=3.
+x=1;y=2;z=3
+print(x,y,z)
+
+#count letters
+x="this is a python practice folder"
+print(x.count('a'))
